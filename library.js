35e23612fee87c462aeff8be81eff0c7ecd27a20
silveryseven <jeiypaltera@gmail.com>
2014-12-07 07:14:22 +0800

    Update library.js

--- library.js
+++ library.js
@@ -1,18 +1,13 @@
 (function() {
 	"use strict";
 	var Color = {},
-		Remarkable = require('remarkable'),
-		parser,
 		html = '<span style="color: $1">$2</span>';
 
 
 	Color = {
 		onLoad: function (params, callback) {
-			Color.init();
+
 			callback();
-		},
-		init: function () {
-			parser = new Remarkable();
 		},
 		parsePost: function(data, callback) {
 			var match = /%\((#(?:[A-Fa-f0-9]{3}(?:[A-Fa-f0-9]{3})?)|(?:rgb\(\d{1,3},\d{1,3},\d{1,3}\))|(?:[a-z]){3,})\)\[(.+?)\]/g;
@@ -20,7 +15,7 @@
 			if (data && data.postData && data.postData.content) {
 				data.postData.content = data.postData.content.replace(match, html);
 				
-				data.postData.content = parser.render(data.postData.content);
+				
 			}
 
 			callback(null, data);	
@@ -32,7 +27,10 @@
 			callback(null, data);
 		},
 		parseRaw: function (raw, callback) {
-			callback(null, raw ? parser.render(raw) : raw);
+
+			var match = /%\((#(?:[A-Fa-f0-9]{3}(?:[A-Fa-f0-9]{3})?)|(?:rgb\(\d{1,3},\d{1,3},\d{1,3}\))|(?:[a-z]){3,})\)\[(.+?)\]/g;			
+
+			callback(null, raw ? raw.replace(match, html) : raw);
 		}
 	};
 
